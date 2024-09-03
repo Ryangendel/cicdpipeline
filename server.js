4839635bdd367b51d8d61cc7b19293bdc3b5a1be
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2024-09-02 18:40:45 -0600

    second commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const Arithmetic = require('./arithmetric.js');
+
+const value = new Arithmetic(4)
+  .plus(8)
+  .plus(15)
+  .minus(16)
+  .minus(23)
+  .plus(42)
+  .plus(108)
+  .value();
+
+console.log(value);
